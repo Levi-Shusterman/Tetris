94c04a363e44a32c66143460504f51cf9b7b34e5
Levi Shusterman <levi.shusterman@gmail.com>
2016-04-23 13:41:40 -0500

    Refining the gui to handle key events. Got timer to work.

--- src/GUI/Tetris.java
+++ src/GUI/Tetris.java
@@ -1,15 +1,26 @@
 package GUI;
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import javax.swing.Timer;
+
 
 
 public class Tetris extends JFrame {
@@ -28,40 +39,99 @@ public class Tetris extends JFrame {
 	
 	// The grid that holds individual positions
 	private JPanel gameGrid;
-	private JLabel[][] positions;
+	private JButton[][] positions;
+	private GridLayout board;
+	
+	// Timer
+	private JLabel timeLabel;
+	private JLabel helpLabel;
+	private JPanel timerPanel;
+	private Timer timer; 
+	Integer count;
 	
 	public Tetris(){
 		super("Tetris");
 		container = getContentPane();
 		
-		// Overall layout of the GUI
-		guiPanel = new JPanel();
-	    guiPanel.setLayout((LayoutManager) new BoxLayout(guiPanel, BoxLayout.PAGE_AXIS));
-	    container.add(guiPanel, BorderLayout.AFTER_LINE_ENDS);
+	    /*
+	     * Set up timer
+	     */
+	    count = 0;
+	    timeLabel = new JLabel();
+	    helpLabel = new JLabel("Press 'h' for help");
+	  
+	    timer = new Timer(2000, new timerAction());
+	    timer.start();
+	    timerPanel = new JPanel();
+	    timerPanel.add(timeLabel, BorderLayout.EAST );
+	    timerPanel.add(helpLabel, BorderLayout.WEST);
+	    container.add(timerPanel, BorderLayout.PAGE_START);
 	    
-//	     add the game grid that will contain the pieces
-//	    gameGrid = new JPanel();
-//	    gameGrid.setLayout(new GridLayout(ROWS , COLS));
-//	    positions = new JLabel[ROWS][COLS];
-//	    
-//	    for( int i = 0; i < ROWS; i++)
-//	    	for(int j = 0; j < ROWS; j++){
-//	    		positions[i][j] = new JLabel();
-//	    		positions[i][j].setBackground(Color.black);
-////	    		positions[i][j].setVisible(true);
-//	    		gameGrid.add(positions[i][j]);
-//	    	}
-//	
-//	    container.add(gameGrid, BorderLayout.CENTER);
-//	    gameGrid.setVisible(true);
-//	    
+	    // Overall layout of the GUI
+	    guiPanel = new JPanel();
+	    guiPanel.setLayout((LayoutManager) new BoxLayout(guiPanel, BoxLayout.PAGE_AXIS));
+	    container.add(guiPanel, BorderLayout.AFTER_LAST_LINE);
+	    
+	    // Add the game grid that will contain the pieces
+	    gameGrid = new JPanel();
+	    board = new GridLayout(ROWS , COLS);
+	    gameGrid.setLayout(board);
+	    positions = new JButton[ROWS][COLS];
+	    
+	    for( int i = 0; i < ROWS; i++)
+	    	for(int j = 0; j < COLS; j++){
+	    		positions[i][j] = new JButton();
+	    		positions[i][j].setBackground(Color.black);
+	    		positions[i][j].setVisible(true);
+	    		gameGrid.add(positions[i][j]);
+	    	}
+	
+	    container.add(gameGrid, BorderLayout.CENTER);
+	    gameGrid.setVisible(true);
+	    
+	    
 	    setSize(700,700);
 	    setVisible( true );
+	    addKeyListener(new buttonAction());
 	}
 	
 	public static void main(String[] args) {
 		Tetris gui = new Tetris();
 	    gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	}
+	
+	private class timerAction implements ActionListener{
+		timerAction(){
+			super();
+		}
+		@Override
+    	public void actionPerformed(ActionEvent e) {
+    		timeLabel.setText(count.toString());
+    		count += 1;
+    	}
+		
+	}
+	
+	private class buttonAction extends KeyAdapter{
+		buttonAction(){
+			super();
+		}
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if( e.getKeyChar() == 'h' ){
+				timer.stop();
+				
+				
+				 JOptionPane.showMessageDialog(null, 
+						"H for help\n"+
+						"Left arrow to shift left\n",
+						"How to play Tetris",
+						JOptionPane.INFORMATION_MESSAGE
+						);
+				 timer.start();
+			}
+		}
+	
 	}
 }
